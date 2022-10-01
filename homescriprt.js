4748329c2be685b38d035a968c7b0611768a2db4
utdsi <utkarshsinha852@gmail.com>
2022-10-01 18:29:57 +0530

    day-5

---
+++ homescriprt.js
@@ -0,0 +1,18 @@
+document.querySelector("#navtop > a").addEventListener("click",navtopfun)
+
+function navtopfun(){
+    window.location.href="./home.html"
+}
+document.querySelector("#log>a").addEventListener("click",loginfun)
+
+function loginfun(){
+    window.location.href="./login.html"
+}
+
+document.getElementById("btn").addEventListener("click",signup1Fun)
+
+function signup1Fun(){
+    window.location.href="./signup.html"
+}
+
+
---
+++ signup.js
@@ -0,0 +1,5 @@
+document.querySelector(".t1>a").addEventListener("click",homeFun)
+
+function homeFun(){
+   window.location.href="./home.html"
+}
